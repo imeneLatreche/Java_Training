851e94774398be38f7c42e49c2adaeba4bbdae85
imeneLatreche <100303346+imeneLatreche@users.noreply.github.com>
2022-02-23 22:45:12 -0600

    Update Question1.java

--- Assignment_1/src/Question1.java
+++ Assignment_1/src/Question1.java
@@ -3,8 +3,8 @@ public class Question1 {
 
 	public static void main(String[] args) {
 		// 1.	Set up a project in Eclipse and print out 
-		//“Hello, my name is ___” in the console
-		System.out.println("Hello, my name is Abdelouahed MANAA");
+		//â€œHello, my name is ___â€ in the console
+		System.out.println("Hello, my name is Imene Latreche");
 
 	}
 
